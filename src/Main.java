9f2e59422b91dbbcaa1efef93b5247b570b34a27
DESKTOP-37L5D6P\Claudiu <cocanclaudiu18@stud.ase.ro>
2022-11-05 07:20:02 +0200

    schimbare meniu 1

--- src/Main.java
+++ src/Main.java
@@ -11,7 +11,7 @@ public class Main {
         PreluareDate preluareDate = new PreluareDate();
         Meniu m1 = new Meniu();
 //
-        preluareDate.citireDate("banci.txt","clienti.txt","tranzactii.txt");
+        preluareDate.citireDate("banci.txt", "clienti.txt", "tranzactii.txt");
         preluareDate.SincronizareDateInput();
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -21,68 +21,72 @@ public class Main {
         System.out.println("x - Intrati in aplicatie, c- Parasiti aplicatia");
 
         int consoleLvl = 0;
-        while (consoleIn.hasNext()) {
-            if (!consoleIn.next().equals("c")) {
 
-                switch (consoleLvl) {
-
-                    case 0: {
-                        m1.afisareListaBanci(preluareDate);
-                        System.out.println();
-                        System.out.println("Selectati banca dorita");
-
-                        try {
-//                            if(consoleIn.next().equals("+")){
-//                                m1.adaugaBanca();
-//                            }else {
-                                int indexBanca = consoleIn.nextInt();
-                                System.out.println("--------------------------------");
-                                m1.afisareClienti(preluareDate.listaBanciCitite.get(indexBanca));
-
-                                consoleLvl = 1;
-                                m1.afisareOptiuniNavigare();
-                            }
-//  }
-
-                        catch (Exception e){
-
-                            System.out.println("Ati introdus o optiune invalida");
-                            System.out.println("Introduceti orice tasta pentru a va intoarce");
-                        }
-
-                        break;
-                    }
-                    default:
-                        System.out.println("Entered default");
-
-                    case 1:
-                        int indexClient = consoleIn.nextInt();
-                        m1.afisareDateClient(preluareDate.listaClientiCititi.get(indexClient));
-
-                }
-
-                if (consoleIn.next().equals("b")) {
-                    consoleLvl--;
-
-                }
-
-            }
-
-            else{
-                return;
-            }
-        }
-
-//                if(consoleIn.next().equals("c")){
-//                    System.out.println("Aplicatia a fost inchisa");
+//        while (1 == 1) {
+//
+//            switch (consoleIn.next()) {
+//                case "c": {
 //                    return;
 //                }
+//                case "b": {
+//                    consoleLvl--;
+//                }
 //
-//                else
+//                default: {
+//                    switch (consoleLvl) {
+//                        case 0: {
+//                            m1.afisareListaBanci(preluareDate);
+//                            System.out.println();
+//                            System.out.println("Selectati banca dorita");
+//
+//                            try {
+////                            if(consoleIn.next().equals("+")){
+////                                m1.adaugaBanca();
+////                            }else {
+//                                int indexBanca = consoleIn.nextInt();
+//                                System.out.println("--------------------------------");
+//                                m1.afisareClienti(preluareDate.listaBanciCitite.get(indexBanca));
+//
+//                                consoleLvl = 1;
+//                                m1.afisareOptiuniNavigare();
+//
+//                            }
+////  }
+//                            catch (Exception e) {
+//
+//                                System.out.println("Ati introdus o optiune invalida");
+//                                System.out.println("Introduceti orice tasta pentru a va intoarce");
+//                            }
+//
+//                            break;
+//                        }
+//                        default:
+//                            System.out.println("Entered default");
+//                            break;
+//
+//                        case 1:
+//                            int indexClient = consoleIn.nextInt();
+//                            m1.afisareDateClient(preluareDate.listaClientiCititi.get(indexClient));
+//                            consoleLvl = 2;
+//                            break;
+//
+//                    }
+//
+//                }
+//
+//            }
+//        }
+
+        m1.afisareMeniuBanci(preluareDate);
+        //m1.afisareMeniuClienti(preluareDate);
             }
+//afara main
 
 
-    }
+
+
+
+}
 
 
 //dobanda banca
--- src/Meniu.java
+++ src/Meniu.java
@@ -1,4 +1,5 @@
 import java.io.IOException;
+import java.util.Scanner;
 
 public class Meniu {
 
@@ -15,19 +16,55 @@ public class Meniu {
 
     }
 
+    public void afisareMeniuBanci(PreluareDate preluareDate) {
+        this.afisareListaBanci(preluareDate);
+        Scanner consoleIn = new Scanner(System.in);
+
+        System.out.println();
+        System.out.println("Selectati banca dorita sau parasiti aplicatia folosind tasta c");
+
+        try {
+            int indexBanca = consoleIn.nextInt();
+            this.afisareClienti(preluareDate.listaBanciCitite.get(indexBanca));
+            //this.NavigareBanci(preluareDate);
+            System.out.println("c- Parasiti aplicatia");
+
+        } catch (Exception e) {
+            if(consoleIn.next().equals("c"))
+                return;
+            else {
+                System.out.println("Ati introdus o optiune invalida");
+
+                afisareMeniuBanci(preluareDate);
+            }
+        }
+            consoleIn.close();
+
+    }
+
+
+
+public void afisareMeniuClienti(PreluareDate preluareDate){
+    Scanner consoleIn = new Scanner(System.in);
+    int indexClient = consoleIn.nextInt();
+    this.afisareDateClient(preluareDate.listaClientiCititi.get(indexClient));
+    NavigareGenerala(preluareDate);
+
+}
+
     public void afisareClienti(Banca banca) throws IOException {
         int i = 0;
+        System.out.println("--------------------------------");
         System.out.println("Clienti:");
         for (Client client : banca.getListaClienti()) {
 
             System.out.println(i+") "+client.getNume());
             i++;
         }
-
-
     }
 
     public void afisareDateClient(Client c){
+        System.out.println("--------------------------------");
         System.out.println("Nume: "+ c.getNume());
         System.out.println("Sold: "+ c.getSold());
         String s;
@@ -42,7 +79,7 @@ public class Meniu {
 
         System.out.println("Status: "+s);
 
-        System.out.println("Banca:"+c.getNumeBanca());
+        System.out.println("Banca: "+c.getNumeBanca());
 
 
     }
@@ -53,15 +90,43 @@ public class Meniu {
     }
 
 
-    public void afisareOptiuniNavigare(){
-        System.out.println();
-        System.out.println("b - inapoi, c - inchide aplicatia ");
-    }
-
     public void adaugaBanca() {
         System.out.println("--------------------------------");
         System.out.println("Adaugare Banca");
         System.out.println();
         System.out.println("Adauga denumire");
     }
+
+    public void NavigareBanci(PreluareDate preluareDate){
+        System.out.println("--------------------------------");
+        System.out.println("c- Parasiti aplicatia");
+        Scanner consoleIn = new Scanner(System.in);
+
+        switch (consoleIn.next()){
+            case "c": return;
+
+            default:
+                System.out.println("--------------------------------");
+                System.out.println("Optiune inexistenta");
+                NavigareBanci(preluareDate);
+
+        }
+    }
+
+
+    public void NavigareGenerala(PreluareDate preluareDate){
+        System.out.println("--------------------------------");
+        System.out.println("b - Inapoi, c- Parasiti aplicatia");
+        Scanner consoleIn = new Scanner(System.in);
+
+        switch (consoleIn.next()){
+            case "c": return;
+            case "b": afisareMeniuBanci(preluareDate);
+            default:
+                System.out.println("--------------------------------");
+                System.out.println("Optiune inexistenta");
+                NavigareGenerala(preluareDate);
+        }
+    }
+
 }
