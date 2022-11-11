d715dfe8f0e63d979c8d1f91081dbcefcc15d9ad
DESKTOP-37L5D6P\Claudiu <cocanclaudiu18@stud.ase.ro>
2022-11-11 21:36:16 +0200

    final(last_testing)

--- src/Tranzactie.java
+++ src/Tranzactie.java
@@ -11,6 +11,7 @@ public class Tranzactie {
     private String deLa;
 
 
+
     public Tranzactie(Tip tipTranzactie, Double suma, String expeditor) {
         this.tipTranzactie = tipTranzactie;
         this.suma = suma;
